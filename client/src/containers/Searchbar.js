0042b1ad517db07446831840bc1ddad496890b07
Peter Choi <phc284@gmail.com>
2018-04-06 15:18:39 -0500

    redux form submit working on searchbar

--- client/src/containers/Searchbar.js
+++ client/src/containers/Searchbar.js
@@ -2,21 +2,24 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Button, TextField } from 'material-ui';
 
-const renderTextField = ({ labelText }) => (
-  <TextField fullWidth name="search" label={labelText} />
+const renderTextField = ({ labelText, input }) => (
+  <TextField fullWidth name="search" label={labelText} {...input} />
 );
 
-const SearchBar = () => {
+const SearchBar = props => {
   const labelText = 'Search for your favorite Oreo...';
+  const onSubmit = values => {
+    console.log('searchbar', values);
+  };
   return (
-    <form className="searchbar">
+    <form onSubmit={props.handleSubmit(onSubmit)} className="searchbar">
       <Field
         name="inputSearch"
         labelText={labelText}
         component={renderTextField}
         type="text"
       />
-      <Button>
+      <Button type="submit">
         <i className="material-icons">search</i>
       </Button>
     </form>
