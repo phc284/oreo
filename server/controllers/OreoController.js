469e51f0c79e627b8250b7ebc6e7b7a91f209722
Peter Choi <phc284@gmail.com>
2018-04-07 20:45:44 -0500

    tags saving in model

--- server/controllers/OreoController.js
+++ server/controllers/OreoController.js
@@ -8,17 +8,27 @@ const window = new JSDOM('').window;
 const DOMPurify = createDOMPurify(window);
 
 exports.createOreo = async (req, res) => {
-  console.log('create oreo', req.body);
+  console.log('/add', req.body);
 
+  const { name, description, photo, ...bodyTags } = req.body;
+  const tags = Object.keys(bodyTags);
+
+  //TODO
   //create new object with only name, description, and key
-
-  const oreo = await new Oreo(req.body).save();
+  let newBody = {
+    name,
+    description,
+    photo,
+    tags
+  };
+  //create an array with the list of tags selected to add to model
+  const oreo = await new Oreo(newBody).save();
 
   res.send(oreo);
 };
 
 exports.getOreos = async (req, res) => {
-  console.log('get all oreos');
+  console.log('/oreos');
 
   //get all the oreos from the database
   const oreos = await Oreo.find();
