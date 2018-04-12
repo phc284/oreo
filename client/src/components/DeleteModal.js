03fbf6709b823440256e96f1f1681b1fc09f3a59
Peter Choi <phc284@gmail.com>
2018-04-12 15:36:53 -0500

    change deletemodal button colors

--- client/src/components/DeleteModal.js
+++ client/src/components/DeleteModal.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Button from 'material-ui/Button';
-import Dialog, { DialogTitle, DialogActions } from 'material-ui/Dialog';
+import React from "react";
+import Button from "material-ui/Button";
+import Dialog, { DialogTitle, DialogActions } from "material-ui/Dialog";
 
 const DeleteModal = props => {
   return (
     <Dialog open={props.isOpen} onClose={props.handleClose}>
       <DialogTitle>Are You Sure You Want to Delete This Cookie?</DialogTitle>
       <DialogActions>
-        <Button color="secondary" onClick={props.handleClose}>
+        <Button color="primary" onClick={props.handleClose}>
           No
         </Button>
-        <Button color="primary" onClick={() => props.handleDelete(props.id)}>
+        <Button color="secondary" onClick={() => props.handleDelete(props.id)}>
           Yes
         </Button>
       </DialogActions>
