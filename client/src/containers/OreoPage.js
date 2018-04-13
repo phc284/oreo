28146b60f8da756581c8ce482dbd8a751bb544d6
Peter Choi <phc284@gmail.com>
2018-04-13 16:02:14 -0500

    add eslint

--- client/src/containers/OreoPage.js
+++ client/src/containers/OreoPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Typography from "material-ui/Typography";
+// import Typography from "material-ui/Typography";
 // import { bindActionCreators } from 'redux';
 import axios from "axios";
 
